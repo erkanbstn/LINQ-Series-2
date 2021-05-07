3160a839e0a2ac7574dc760aff9bbc0a35c5c9fc
Erkan <profesyonel59@gmail.com>
2021-05-07 21:38:09 +0300

    Linq Work Project
    Linq Sorguları Çalışma Projesi

---
+++ Linq Sorguları 2/Linq Sorguları 2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Linq_Sorguları_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblmin = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblmax = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(554, 178);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button1.Location = new System.Drawing.Point(572, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LİNQ Entity";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblmin
+            // 
+            this.lblmin.AutoSize = true;
+            this.lblmin.Location = new System.Drawing.Point(142, 196);
+            this.lblmin.Name = "lblmin";
+            this.lblmin.Size = new System.Drawing.Size(110, 27);
+            this.lblmin.TabIndex = 3;
+            this.lblmin.Text = "Minimum";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 34);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblmax
+            // 
+            this.lblmax.AutoSize = true;
+            this.lblmax.Location = new System.Drawing.Point(13, 196);
+            this.lblmax.Name = "lblmax";
+            this.lblmax.Size = new System.Drawing.Size(110, 27);
+            this.lblmax.TabIndex = 5;
+            this.lblmax.Text = "Maximum";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(580, 95);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(173, 31);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Gruplandır";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Kalanlardan Max";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 27);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Geçenlerden Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 27);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Veri Sayısı ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 27);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Not Toplam";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(278, 27);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Adı Erkan Olan Sayı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(481, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(278, 27);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "En Yüksek Not Değer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(765, 370);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.lblmax);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblmin);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Linq Sorguları 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblmin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblmax;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Linq Sorguları 2/Linq Sorguları 2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linq_Sorguları_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DBSınavEntityEntities db = new DBSınavEntityEntities();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblmax.Text = db.TblNotlar.Max(x=>x.Ortalama).ToString();
+
+            lblmin.Text = db.TblNotlar.Min(x=>x.Ortalama).ToString();
+
+            label1.Text = db.TblNotlar.Where(x => x.Durum == false).Max(x => x.Ortalama).ToString();
+
+            label2.Text = db.TblNotlar.Where(x => x.Durum == true).Min(x => x.Ortalama).ToString();
+
+            label3.Text = db.TblÖğrenci.Count().ToString();
+
+            label4.Text = db.TblNotlar.Sum(x => x.Sınav1).ToString();
+
+            label5.Text = db.TblÖğrenci.Count(x=>x.Ad=="Erkan").ToString();
+
+            label6.Text = (from x in db.TblNotlar orderby x.Sınav1 descending select x.Ders).First().ToString();
+
+            dataGridView2.DataSource = db.notta().ToList();
+
+
+            if (radioButton1.Checked == true)
+            {
+                //var s = db.TblÖğrenci.
+                //    OrderBy(x => x.Şehir).
+                //    GroupBy(y => y.Şehir).
+                //    Select(v => new { Şehir = v.Key, Toplam = v.Count() }).
+                //    OrderByDescending(c=>c.Toplam);
+                //dataGridView2.DataSource = s.ToList();
+            }
+            if (radioButton1.Checked == true)
+            {
+                var x = db.TblÖğrenci.OrderBy(g => g.Şehir).GroupBy(z => z.Şehir).Select(v => new { Şehir = v.Key, Toplam = v.Count() }).OrderByDescending(b=>b.Toplam);
+                dataGridView2.DataSource = x.ToList();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
